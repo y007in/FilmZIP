0bf49aed28e78244179cc75939cafa5265eb64ab
yoojin <yoojin4719@gmail.com>
2024-12-23 23:49:53 +0900

    feat : 검색폼 구현하기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [searchKeyword, setSearchKeyword] = useState("");
+
+  const handleSearchKeyword = (e) => {
+    if (e.target.value.length <= 0) {
+      handleReset();
+    }
+    setSearchKeyword(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submit :", searchKeyword);
+  };
+  const handleReset = () => {
+    setSearchKeyword("");
+  };
+  useEffect(() => {
+    if (searchKeyword === "") {
+      console.log("Reset :", searchKeyword);
+    }
+  }, [searchKeyword]);
+  return (
+    <div className="App">
+      <header>
+        <h2>검색</h2>
+      </header>
+      <div className="container">
+        <form onSubmit={(e) => handleSubmit(e)} onReset={handleReset}>
+          <input
+            className="searchInput"
+            type="text"
+            placeholder="검색어를 입력하세요."
+            autoFocus
+            value={searchKeyword}
+            onChange={(e) => handleSearchKeyword(e)}
+          />
+          {searchKeyword.length > 0 && (
+            <button type="reset" className="btnReset"></button>
+          )}
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
