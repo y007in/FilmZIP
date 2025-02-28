6cc84fa9d4cbbed9bf513d151985c0f39f7ffa07
yoojin <yoojin4719@gmail.com>
2025-02-28 15:05:02 +0900

    feat : completedOrder Page 레이아웃 및 스타일링 완료

--- src/constants/paymentMethod.js
+++ src/constants/paymentMethod.js
@@ -1,6 +1,1 @@
-export const paymentMethod = [
-  '신용카드',
-  '네이버페이',
-  '카카오페이',
-  '계좌이체',
-];
+export const paymentMethod = ['신용카드', '네이버페이', '카카오페이'];
--- src/pages/CompletedOrder/CompletedOrder.js
+++ src/pages/CompletedOrder/CompletedOrder.js
@@ -1,7 +1,64 @@
 import React from 'react';
+import Page from '../../components/Page/Page';
+import Header from '../../components/Header/Header';
+import MovieList from '../../components/MovieList/MovieList';
+import Button from '../../components/Button/Button';
+import { useNavigate } from 'react-router-dom';
 
 const CompletedOrder = () => {
-  return <div>주문완료</div>;
+  const cartList = JSON.parse(localStorage.getItem('movie')) || [];
+  const navigate = useNavigate();
+
+  const data = [
+    { label: '예매번호', content: '202500000001' },
+    {
+      label: '예매일',
+      content: '2025.00.00',
+    },
+
+    { label: '예매자명', content: '홍길동' },
+    {
+      label: '연락처',
+      content: '010-1234-1234',
+    },
+    {
+      label: '결제방법',
+      content: '카드결제',
+    },
+    {
+      label: '예매목록',
+      content: `${cartList[0].title}`,
+    },
+  ];
+  return (
+    <div className="CompletedOrder">
+      <Page header={<Header back />}>
+        <div className="orderContainer">
+          <div className="bookInfo">
+            <h1>결제가 완료되었습니다.</h1>
+            {data.map(book => (
+              <div className="personalBook">
+                <p className="bookLabel">{book.label}</p>
+                <p className="bookContent">{book.content}</p>
+              </div>
+            ))}
+            <Button
+              text={'홈으로'}
+              styleSize={'small'}
+              styleType={'full'}
+              onClick={() => navigate('/')}
+            />
+          </div>
+          <ul className="precautions">
+            <li>
+              ※ 구매 후 시청하시기 전에 구매일로부터 7일 이내에 구매 취소
+              가능합니다.
+            </li>
+          </ul>
+        </div>
+      </Page>
+    </div>
+  );
 };
 
 export default CompletedOrder;
