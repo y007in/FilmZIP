719ea5943943c0a899de3c1980dfc67aa2f93988
yoojin <yoojin4719@gmail.com>
2025-05-20 15:35:28 +0900

    style : 다크모드시 banner component 색상 조정

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,5 +1,8 @@
 const Banner = ({ text, bannerType }) => {
-  return <span className={[`banner ${bannerType}`]}>{text}</span>;
+  const ban_type = ['brandSolid', 'brand', 'sub'].includes(bannerType)
+    ? bannerType
+    : '';
+  return <span className={[`banner ${ban_type}`]}>{text}</span>;
 };
 
 export default Banner;
