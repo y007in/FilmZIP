a585346b14d12211ab47eb4fc94d29022e7eabe3
yoojin <yoojin4719@gmail.com>
2025-06-18 01:11:08 +0900

    refactor : movieDetail/Image/Credit/Release API 요청 공통 로직 movieIdFetch로 통합

--- src/api/api.js
+++ src/api/api.js
@@ -9,45 +9,30 @@ const fetchFromApi = async endpoint => {
   return response.json();
 };
 
-export const fetchMovie = async page => {
-  return fetchFromApi(`/movie/now_playing?language=ko-KR&page=${page}`);
-};
 export const fetchTopRated = async () => {
   return fetchFromApi(`/movie/popular?language=ko-KR&page=1`);
 };
 
-export const fetchSearch = async ({ queryKey }) => {
-  const [, searchKeyword] = queryKey;
-  if (!searchKeyword) return [];
-  return fetchFromApi(`/search/movie?query=${searchKeyword}&language=ko-KR}`);
-};
-
-export const fetchMovieDetail = async ({ queryKey }) => {
-  const [, movieId] = queryKey;
-  if (!movieId) return [];
-  return fetchFromApi(`/movie/${movieId}?language=ko-KR`);
-};
+//장르 목록
 export const fetchGenre = async () => {
   return fetchFromApi(`/genre/movie/list?language=ko-KR`);
 };
+export const fetchSearch = async ({ queryKey }) => {
+  const [, searchKeyword] = queryKey;
+  if (!searchKeyword) return [];
+  return fetchFromApi(`/search/movie?query=${searchKeyword}&language=ko-KR`);
+};
 
-export const fetchMovieVideo = async ({ queryKey }) => {
-  const [, movieId] = queryKey;
-  if (!movieId) return [];
-  return fetchFromApi(`/movie/${movieId}/videos?language=ko-KR`);
-};
-export const fetchMovieImage = async ({ queryKey }) => {
-  const [, movieId] = queryKey;
-  if (!movieId) return [];
-  return fetchFromApi(`/movie/${movieId}/images`);
-};
-export const fetchMovieCredit = async ({ queryKey }) => {
-  const [, movieId] = queryKey;
-  if (!movieId) return [];
-  return fetchFromApi(`/movie/${movieId}/credits`);
-};
-export const fetchMovieRelease = async ({ queryKey }) => {
-  const [, movieId] = queryKey;
-  if (!movieId) return [];
-  return fetchFromApi(`/movie/${movieId}/release_dates`);
-};
+//영화 정보
+const movieIdFetch =
+  path =>
+  async ({ queryKey }) => {
+    const [, movieId] = queryKey;
+    if (!movieId) return [];
+    return fetchFromApi(`/movie/${movieId}${path}`);
+  };
+
+export const fetchMovieDetail = movieIdFetch(`?language=ko-KR`);
+export const fetchMovieImage = movieIdFetch(`/images`);
+export const fetchMovieCredit = movieIdFetch(`/credits`);
+export const fetchMovieRelease = movieIdFetch(`/release_dates`);
